3bee8a63c0f2934ab2a34fd390e867b3104995e4
aarya526 <aarya526@gmail.com>
2019-11-03 00:46:19 +0530

    project task, backlog

--- hakke-ppmtool-react/src/App.js
+++ hakke-ppmtool-react/src/App.js
@@ -9,6 +9,7 @@ import { Provider } from "react-redux";
 import store from './store';
 import UpdateProject from "./components/Project/UpdateProject";
 
+
 class App extends Component {
   render() {
     return (
